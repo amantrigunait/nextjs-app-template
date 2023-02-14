2b03306f16d12f7ba8e0c6dbca3c8fb2675ab720
Aman Trigunait <amantrigunait.iitk@gmail.com>
2023-02-14 17:06:44 +0530

    build: added layout

---
+++ src/components/navigation/header/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
+
+const Header: React.FC<IHeader> = ({ className, ...headerProps }) => {
+  return (
+    <header
+      {...headerProps}
+      className={`w-full flex flex-row justify-between ${className}`}
+    >
+      <div className="space-x-5 m-5">
+        <Link href="/">Home</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
